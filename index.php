73e465f5d93e51500af58f43e88405f105fd0ee4
Wulan Amanda <wulan@vinceredigital.com>
2022-08-31 14:49:29 +0800

    added lists

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates atque, corrupti aliquid ex neque non rerum quos tenetur voluptatibus quis corporis sed odit? Vero saepe a mollitia vitae itaque. Aut?</p>
+    <ul>
+        <li>one</li>
+        <li>two</li>
+    </ul>
 </body>
 </html>
